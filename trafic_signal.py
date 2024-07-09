82dd9634e9205237d934766de969b035c92b9fe7
tarun0714 <manipalmemories1407@gmail.com>
2024-07-09 23:48:25 +0530

    traffic_loop

---
+++ trafic_signal.py
@@ -0,0 +1,10 @@
+light = input ("light colour : ")
+
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("wait")
+elif(light == "green"):
+    print("go")
+else:
+    print("Light is broken")            
