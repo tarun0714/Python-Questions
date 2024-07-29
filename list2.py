78dffc2b3beb14596d0a72931b4aa8001aec2072
tarun0714 <manipalmemories1407@gmail.com>
2024-07-30 01:15:52 +0530

    list2

---
+++ list2.py
@@ -0,0 +1,12 @@
+#WAP to check if a list contains a palindrome of elements. (Hint: use copy( ) method)
+
+list1 = [1,2,1]
+list2 = [1,2,3]
+
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 == list1):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
