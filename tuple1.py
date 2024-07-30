deb00fb823fa61f01535020366212356e1a00341
tarun0714 <manipalmemories1407@gmail.com>
2024-07-30 13:37:14 +0530

    tuples

---
+++ tuple1.py
@@ -0,0 +1,5 @@
+#WAP to count the number of students with the "A" grade in the following tuple.
+
+tuple = ('2','3','8','2','2')
+
+print (tuple.count('2'))
---
+++ tuple2.py
@@ -0,0 +1,7 @@
+#Store the above values in a list & sort them from "A" to "D".
+
+tuple = ['A','B','A','C','D','C','A']
+
+tuple.sort()
+
+print (tuple)
